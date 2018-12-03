54d295829f68ea176dbadaac54b87f8d43a09829
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-12-03 05:02:08 -0800

    production

--- knexfile.js
+++ knexfile.js
@@ -9,29 +9,12 @@ module.exports = {
     useNullAsDefault: true,
     debug: true
   },
-
-  staging: {
+  production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
     useNullAsDefault: true
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
 }
