31f7b75630f5417d7424b21ea95d5ad9d9ed92b9
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-12-03 14:44:49 -0800

    merger conflict

---
+++ client/build/precache-manifest.d50e8fa2112af66d16012534d9fdb427.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6874d609e7338080bcac",
+    "url": "/static/js/main.6874d609.chunk.js"
+  },
+  {
+    "revision": "2c9a509584b5e1ff5b57",
+    "url": "/static/js/1.2c9a5095.chunk.js"
+  },
+  {
+    "revision": "fb8a3da9040a940179b632276ba873fb",
+    "url": "/index.html"
+  }
+];
