fbe8fa5a5d2f6119d22be82d4b0d64d8dd73ef93
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-12-03 05:52:15 -0800

    final fix

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,8 +7,8 @@ const {
   logout,
   socialLogin
 } = require('../controllers/authController')
-const redirectURL = 'http://localhost:3000/signin'
-
+// const redirectURL = 'http://localhost:3000/signin'
+const redirectURL = 'https://practical-shirley-5ebe20.netlify.com/signin'
 //* Local Middleware
 const checkCredentials = require('../middleware/checkCredentials')
 
