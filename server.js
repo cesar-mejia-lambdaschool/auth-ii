8ffcf31220afa41775026232f031ceb04642f82b
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-12-03 05:45:39 -0800

    server online

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const server = require('express')()
-// const errorHandler = require('./helpers/errorHandler')
+const errorHandler = require('./helpers/errorHandler')
 
-// //* Middleware & Routes
-// require('./middleware')(server)
-// require('./routes')(server)
+//* Middleware & Routes
+require('./middleware')(server)
+require('./routes')(server)
 
-// //* Error Handler
-// server.use(errorHandler)
+//* Error Handler
+server.use(errorHandler)
 
 module.exports = server
